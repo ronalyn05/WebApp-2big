e62f6fb01e7a75d103982fdb77f6a9a05f2708b0
ronalyn05 <rogidmarabi@gmail.com>
2023-03-30 20:36:58 +0800

    Latest changes for March 30, 2023

--- Admin/EmployeeRecord.aspx.designer.cs
+++ Admin/EmployeeRecord.aspx.designer.cs
@@ -276,12 +276,12 @@ namespace WRS2big_Web.Admin
         protected global::System.Web.UI.WebControls.DropDownList drd_empStatus;
 
         /// <summary>
-        /// btnUpdate control.
+        /// btnUpdateData control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnUpdate;
+        protected global::System.Web.UI.WebControls.Button btnUpdateData;
     }
 }
