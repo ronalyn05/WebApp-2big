878b72d7bfff02fbb8b227fcd327053f1f09b9ca
ronalyn05 <rogidmarabi@gmail.com>
2023-06-13 18:34:09 +0800

    Reviews and ratings, negative sign for product, AdminAccount, and Customer.cs

--- Admin/Reviews.aspx.designer.cs
+++ Admin/Reviews.aspx.designer.cs
@@ -24,12 +24,30 @@ namespace WRS2big_Web.Admin
         protected global::System.Web.UI.WebControls.Label Label1;
 
         /// <summary>
-        /// GridView1 control.
+        /// lblAverageRating control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView GridView1;
+        protected global::System.Web.UI.WebControls.Label lblAverageRating;
+
+        /// <summary>
+        /// lblStarIcon control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblStarIcon;
+
+        /// <summary>
+        /// lblReview control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblReview;
     }
 }
